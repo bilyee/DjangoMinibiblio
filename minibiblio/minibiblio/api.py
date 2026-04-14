0cdfebf90357c094d9071b9e0880ccf669ec8c36
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-04-14 20:12:20 +0200

    Update api.py

--- minibiblio/minibiblio/api.py
+++ minibiblio/minibiblio/api.py
@@ -13,9 +13,19 @@ class LlibreOut(Schema):
     autor: str
     data_edicio: datetime.date
     resum: Optional[str]
+    imatge: Optional[str]
  
 @api.get("/llibres", response=List[LlibreOut])
 @api.get("/llibres/", response=List[LlibreOut])
 def obtenir_libres(request):
-    qs = Llibre.objects.all()
-    return qs
+    return [
+        {
+            'id': llibre.id,
+            'titol': llibre.titol,
+            'autor': llibre.autor,
+            'data_edicio': llibre.data_edicio,
+            'resum': llibre.resum,
+            'imatge_principal': llibre.imatge_principal.url if llibre.imatge_principal else None,
+        }
+        for llibre in Llibre.objects.all().order_by('id')
+    ]
