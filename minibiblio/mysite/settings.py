6ee8c4ac31971a4370e5b026e6c4a145d937bcf6
bilyee <super@bmaringonzalez3.ieti.local>
2026-04-14 19:20:31 +0200

    Por fin casi acabado y funcionando

--- minibiblio/mysite/settings.py
+++ minibiblio/mysite/settings.py
@@ -16,28 +16,30 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env(
+    DEBUG=(bool, False),
+)
+environ.Env.read_env(str(BASE_DIR / '.env'))
 
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+env = environ.Env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&sp=v7=rihh^x2rxs49*4=vf7k=%i7qgs28mhqjpz6k%7y_z$$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
-AUTH_USER_MODEL = 'minibiblio.Usuari'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
+ALLOWED_HOSTS = ['bmaringonzalez3.ieti.site']
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "minibiblio.apps.MinibiblioConfig",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -81,13 +83,32 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+CORS_ALLOWED_ORIGINS = env.list(
+    'CORS_ALLOWED_ORIGINS',
+    default=[
+	'http://localhost:5173',
+	'http://127.0.0.1:5173',
+    ],
+)
+
+CSRF_TRUSTED_ORIGINS = env.list(
+    'CSRF_TRUSTED_ORIGINS',
+    default=[
+	'https://bmaringonzalez3.ieti.site',
+    ],
+)
+
+if not DEBUG:
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
@@ -125,24 +146,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'minibiblio.Usuari'
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
     
 # Podeu deixar les instruccions que hi hagi de l'esquelet de Django
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     
 # llegim .env
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS",default=[
-    "http://localhost:5173",    # Exemple: React en desenvolupament amb Vite o CRA
-    "http://127.0.0.1:5173",
-])
