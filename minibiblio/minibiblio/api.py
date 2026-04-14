4c35a99b8212c3b403522150a6a189a3f746dd06
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-04-14 20:06:01 +0200

    Update api.py

--- minibiblio/minibiblio/api.py
+++ minibiblio/minibiblio/api.py
@@ -6,43 +6,16 @@ import datetime
 from .models import *
  
 api = NinjaAPI()
-
-class ImatgeOut(Schema):
-    id: int
-    imatge: Optional[str]
-
-    @staticmethod
-    def resolve_imatge(obj):
-        return obj.imatge.url if obj.imatge else None
  
 class LlibreOut(Schema):
     id: int
     titol: str
     autor: str
-    data_edicio: Optional[datetime.date]
+    data_edicio: datetime.date
     resum: Optional[str]
-    imatge: Optional[str]
  
 @api.get("/llibres", response=List[LlibreOut])
 @api.get("/llibres/", response=List[LlibreOut])
 def obtenir_libres(request):
     qs = Llibre.objects.all()
     return qs
-
-@api.get("/llibres/{llibre_id}/imatges", response=List[ImatgeOut])
-def obtenir_imatges_llibre(request, llibre_id: int):
-    qs = ImatgeLlibre.objects.filter(llibre_id=llibre_id)
-    return qs
-
-def obtenir_libres(request):
-    return [
-        {
-            'id': llibre.id,
-            'titol': llibre.titol,
-            'autor': llibre.autor,
-            'data_edicio': llibre.data_edicio,
-            'resum': llibre.resum,
-            'imatge': llibre.imatge_principal.url if llibre.imatge_principal else None,
-        }
-        for llibre in Llibre.objects.all().order_by('id')
-    ]
